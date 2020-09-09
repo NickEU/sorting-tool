183809ae44b5548719311ba15cc8168a0bad6a7b
Nikolajs Korolkovs <k@nick.tf>
2020-09-09 19:06:44 +0300

    s2: InputStats: fixed generic using out

--- src/sorting/InputStats.kt
+++ src/sorting/InputStats.kt
@@ -1,3 +1,4 @@
 package sorting
 
-class InputStats<T>(val totalCount: Int, val longest: T, val times: Int, val percentage: Int)
+class InputStats<out T>(val totalCount: Int, val longest: T,
+                        val times: Int, val percentage: Int)
