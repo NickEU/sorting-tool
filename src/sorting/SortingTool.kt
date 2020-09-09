8e83e7add2827c33a2f8b9f225347314dd13a6b8
Nikolajs Korolkovs <k@nick.tf>
2020-09-09 18:41:45 +0300

    s2: + word & line CLI command functionality

--- src/sorting/SortingTool.kt
+++ src/sorting/SortingTool.kt
@@ -1,21 +1,54 @@
 package sorting
 
-import java.util.*
+import java.util.Scanner
 import kotlin.streams.toList
 
 class SortingTool(args: Array<String>) {
-    private val inputType : InputType
+    private val inputType: InputType
     private val sc = Scanner(System.`in`)
+
     init {
         inputType = parseArguments(args)
     }
 
     fun run() {
         when (inputType) {
-            InputType.WORD -> println()
-            InputType.LINE -> println()
+            InputType.WORD -> getStatsForStrings("\\s+")
+            InputType.LINE -> getStatsForStrings("\\R")
             InputType.LONG -> getStatsForNumbers()
         }
+    }
+
+    private fun getStatsForStrings(delimiter: String) {
+        val input = sc.useDelimiter(delimiter)
+                .tokens().toList()
+        val totalCount = input.count()
+        val longest = input.maxBy { it.length }
+        val times = input.filter { n -> n == longest }.size
+        val percentage = (100.0 * times / totalCount).toInt()
+
+        printStatsForString(InputStats(totalCount, longest, times, percentage))
+    }
+
+    private fun printStatsForString(stats: InputStats<String?>) {
+        val isWord = inputType == InputType.WORD
+        val type = if (isWord) "word" else "line"
+        val optionalBr = if (isWord) "" else "\n"
+        println("Total ${type}s: ${stats.totalCount}.")
+        println("The longest $type: $optionalBr${stats.longest}" +
+                "$optionalBr (${stats.times} time(s), ${stats.percentage}%).")
+    }
+
+    private fun getStatsForNumbers() {
+        val userNums = sc.tokens()
+                .mapToInt(Integer::parseInt)
+                .toList()
+        val totalCount = userNums.count()
+        val max = userNums.max()
+        val times = userNums.filter { n -> n == max }.size
+        val percentage = (100.0 * times / totalCount).toInt()
+        println("Total numbers: $totalCount.")
+        println("The greatest number: $max ($times time(s), $percentage%).")
     }
 
     private fun parseArguments(args: Array<String>): InputType {
@@ -30,17 +63,5 @@ class SortingTool(args: Array<String>) {
 
             else -> InputType.WORD
         }
-    }
-
-    private fun getStatsForNumbers() {
-        val userNums = sc.tokens()
-                .mapToInt(Integer::parseInt)
-                .toList()
-        val totalCount = userNums.count()
-        val max = userNums.max()
-        val times = userNums.filter{ n -> n == max }.size
-        val percentage = (100.0 * times / totalCount).toInt()
-        println("Total numbers: $totalCount.")
-        println("The greatest number: $max ($times time(s), $percentage%).")
     }
 }
