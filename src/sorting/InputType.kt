53a730abef16ffcdaec7a97bfa957b1bb0c8c625
Nikolajs Korolkovs <k@nick.tf>
2020-09-09 19:35:57 +0300

    cleanup: InputType: refactoring

--- src/sorting/InputType.kt
+++ src/sorting/InputType.kt
@@ -1,10 +1,10 @@
 package sorting
 
-enum class InputType(private val s: String) {
+enum class InputType(private val typeName: String) {
     LONG("number"),
     LINE("line"),
     WORD("word");
 
-    fun getName(): String = s
+    fun getName(): String = typeName
     fun getWordMax(): String = if (this.name == LONG.name) "greatest" else "longest"
 }
