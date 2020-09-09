6607cd04ffc540d48be22e480d88c974e37207ca
Nikolajs Korolkovs <k@nick.tf>
2020-09-09 19:07:17 +0300

    s2: InputType: made enum more useful

--- src/sorting/InputType.kt
+++ src/sorting/InputType.kt
@@ -1,7 +1,10 @@
 package sorting
 
-enum class InputType {
-    LONG,
-    LINE,
-    WORD
+enum class InputType(private val s: String) {
+    LONG("number"),
+    LINE("line"),
+    WORD("word");
+
+    fun getName(): String = s
+    fun getWordMax(): String = if (this.name == LONG.name) "greatest" else "longest"
 }
