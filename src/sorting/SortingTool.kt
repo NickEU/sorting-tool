d003cf90db984216ea3755ef786dc02f8be763d4
Nikolajs Korolkovs <k@nick.tf>
2020-09-10 13:58:23 +0300

    s4: SortingTool: implemented natural sort for nums

--- src/sorting/SortingTool.kt
+++ src/sorting/SortingTool.kt
@@ -6,36 +6,39 @@ import kotlin.streams.toList
 class SortingTool(args: Array<String>) {
     private val config: Config = ArgsParser.buildConfig(args)
     private val sc = Scanner(System.`in`)
-    private var performSort = false
 
     fun run() {
         val whiteSpaceDelimiter = "\\s+"
         val newlineDelimiter = "\\R"
         when (config.inputType) {
-            InputType.WORD -> getStatsForStrings(whiteSpaceDelimiter)
-            InputType.LINE -> getStatsForStrings(newlineDelimiter)
-            InputType.LONG -> getStatsForNumbers()
+            InputType.WORD -> printSortedStrings(whiteSpaceDelimiter)
+            InputType.LINE -> printSortedStrings(newlineDelimiter)
+            InputType.LONG -> printSortedNumbers()
         }
     }
 
-    private fun getStatsForStrings(delimiter: String) {
+    private fun printSortedStrings(delimiter: String) {
         val input = sc.useDelimiter(delimiter)
                 .tokens().toList()
         printStats(InputStats(input, input.maxBy { it.length }))
     }
 
-    private fun getStatsForNumbers() {
+    private fun printSortedNumbers() {
         val userNums = sc.tokens()
                 .mapToInt(Integer::parseInt)
                 .toList()
-        if (performSort) {
+        if (config.sortingType == SortingType.NATURAL) {
             val sortedNums = userNums.sorted()
                     .joinToString(" ")
             println("Total numbers: ${userNums.size}.")
             println("Sorted data: $sortedNums")
-        } else {
-            printStats(InputStats(userNums, userNums.max()))
+        } else if (config.sortingType == SortingType.BY_COUNT){
+            printElementsByCount()
         }
+    }
+
+    private fun printElementsByCount() {
+        TODO("Not yet implemented")
     }
 
     private fun printStats(stats: InputStats<*>) {
