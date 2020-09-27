3c0e683a339d232be38555aaba0351a4d05578fc
Nikolajs Korolkovs <k@nick.tf>
2020-09-27 13:26:56 +0300

    s6: SortingTool: refactoring

--- src/sorting/SortingTool.kt
+++ src/sorting/SortingTool.kt
@@ -4,12 +4,10 @@ import java.io.File
 import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
 import java.util.*
-import java.util.stream.Stream
 import kotlin.streams.toList
 
 class SortingTool {
     private lateinit var config: Config
-    private val sc = Scanner(System.`in`)
 
     fun run(args: Array<String>) {
         try {
@@ -20,26 +18,17 @@ class SortingTool {
         val delimiter = if (config.inputType == InputType.LINE) "\\R" else "\\s+"
         val tokens =
                 if (config.inputFileName.isNotEmpty())
-                    getTokensFromFile(delimiter)
-                else sc.useDelimiter(delimiter).tokens()
+                    File(config.inputFileName).readText().split(delimiter).stream()
+                else Scanner(System.`in`).useDelimiter(delimiter).tokens()
 
         when (config.inputType) {
-            InputType.WORD -> printResultsSelector(tokens.toList())
-            InputType.LINE -> printResultsSelector(tokens.toList())
-            InputType.LONG -> printResultsSelector(stringsToListOfLongs(tokens))
+            InputType.WORD, InputType.LINE -> printResultsSelector(tokens.toList())
+            InputType.LONG -> printResultsSelector(tokens
+                    .filter { s ->
+                        tryParseToLong(s) != null
+                    }.mapToLong(String::toLong)
+                    .toList())
         }
-    }
-
-    private fun getTokensFromFile(delimiter: String): Stream<String> {
-        return File(config.inputFileName).readText().split(delimiter).stream()
-    }
-
-    private fun stringsToListOfLongs(tokens: Stream<String>): List<Long> {
-        return tokens
-                .filter { s ->
-                    tryParseToLong(s) != null
-                }.mapToLong(String::toLong)
-                .toList()
     }
 
     private fun tryParseToLong(token: String): Long? {
